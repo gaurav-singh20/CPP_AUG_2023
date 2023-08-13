dc50ecdd43b93009aeedf6894476c1e33cb04868
Gaurav Singh <120627440+gaurav-singh20@users.noreply.github.com>
2023-08-13 14:16:57 +0530

    func and struct pushed

--- 05_Functions/01_Intro.cpp
+++ 05_Functions/01_Intro.cpp
@@ -1,44 +1,33 @@
-// #include <iostream>
-// using namespace std;
-
-// int myProd(int num1, int num2){
-//     return num1*num2;    
-// }
-
-// int main(){
-//     int n1,n2;
-//     cout << "enter n1: ";
-//     cin >> n1;
-//     cout << "enter n2: ";
-//     cin >> n2;
-//     cout << myProd(n1,n2);
-// }
-
-
-
 #include <iostream>
 using namespace std;
 
-int max_num(int num1,int num2,int num3){
-    if(num1>num2 && num1>num3){
-        return num1;
-    }
-    else if(num2>num3){
-        return num2;
-    }
-    else{
-        return num3;
-    }
+void myMainFunc(){ //Creating/declaring a function
+    cout << "4" << endl; //definig a function
+    int localVar = 20;
+}
+
+void myFunc1(){
+    myMainFunc();
+    cout << "2" << endl;
+}
+
+void myFunc2(){
+    cout << "1" << endl;
+    myFunc1();
+    cout << "3" << endl;
+}
+
+
+// void myFunc(); //declaration
+
+void myFunc(){
+    cout << "Hello" << endl;
 }
 
 int main(){
-    int n1,n2,n3;
-    cout << "enter n1 : ";
-    cin >> n1;
-    cout << "enter n2 : ";
-    cin >> n2;
-    cout << "enter n3 : ";
-    cin >> n3;
-    cout << "max no. is : " << max_num(n1,n2,n3) << endl;
-    cout << "end" << endl;
+    // myFunc2();
+    // cout << "5" << endl;
+
+    myFunc();
+
 }
--- 05_Functions/02_prac.cpp
+++ 05_Functions/02_prac.cpp
@@ -1,15 +1,44 @@
+// #include <iostream>
+// using namespace std;
+
+// int myProd(int num1, int num2){
+//     return num1*num2;    
+// }
+
+// int main(){
+//     int n1,n2;
+//     cout << "enter n1: ";
+//     cin >> n1;
+//     cout << "enter n2: ";
+//     cin >> n2;
+//     cout << myProd(n1,n2);
+// }
+
+
+
 #include <iostream>
 using namespace std;
 
-int myProd(int num1, int num2){
-    return num1*num2;    
+int max_num(int num1,int num2,int num3){
+    if(num1>num2 && num1>num3){
+        return num1;
+    }
+    else if(num2>num3){
+        return num2;
+    }
+    else{
+        return num3;
+    }
 }
 
 int main(){
-    int n1,n2;
-    cout << "enter n1: ";
+    int n1,n2,n3;
+    cout << "enter n1 : ";
     cin >> n1;
-    cout << "enter n2: ";
+    cout << "enter n2 : ";
     cin >> n2;
-    cout << myProd(n1,n2);
+    cout << "enter n3 : ";
+    cin >> n3;
+    cout << "max no. is : " << max_num(n1,n2,n3) << endl;
+    cout << "end" << endl;
 }
