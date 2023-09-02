9e06532d05cbe972de751b5260bdfee2f01db912
Gaurav Singh <120627440+gaurav-singh20@users.noreply.github.com>
2023-09-02 14:01:18 +0530

    sep 2

--- CPP/05_Functions/04_Friend_Function.cpp
+++ CPP/05_Functions/04_Friend_Function.cpp
@@ -1,34 +1,34 @@
 #include<iostream>
 using namespace std;
-class B; //forward declaration
+class class_B; //forward declaration
 
-class A {
+class class_A {
     int data;
 
 public:
     void setValue(int val) {
         data = val;
     }
-    friend void add(A, B);     // B doesn't exits as of now so compiler shows error without forward declaration
+    friend void add(class_A, class_B);     // B doesn't exits as of now so compiler shows error without forward declaration
 };
 
-class B {
+class class_B {
     int data;
 
 public:
     void setValue(int val) {
         data = val;
     }
-    friend void add(A, B);
+    friend void add(class_A, class_B);
 };
 
-void add(A obj1, B obj2) {
+void add(class_A obj1, class_B obj2) {
     cout << "The sum is: " << obj1.data + obj2.data;
 }
 
 int main() {
-    A a1;
-    B b1;
+    class_A a1;
+    class_B b1;
     a1.setValue(10);
     b1.setValue(20);
     add(a1, b1);
