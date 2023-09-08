936744e998fdca7a9a2a56aa38a3169c4e050f77
Gaurav Singh <120627440+gaurav-singh20@users.noreply.github.com>
2023-09-09 02:46:40 +0530

    sep 9

---
+++ CPP/99_prac/4Literals.cpp
@@ -0,0 +1,12 @@
+#include<iostream> 
+
+using namespace std;
+
+int main(){
+    
+    cout<<"The size of 34.4 is "<<sizeof(34.4)<<endl; //double bydefault
+    cout<<"The size of 34.4f is "<<sizeof(34.4f)<<endl;
+    cout<<"The size of 34.4F is "<<sizeof(34.4F)<<endl;
+    cout<<"The size of 34.4l is "<<sizeof(34.4l)<<endl;
+    cout<<"The size of 34.4L is "<<sizeof(34.4L)<<endl;
+}
