7d471f9de0de99e592874373f6879ba22c46b107
Gaurav Singh <120627440+gaurav-singh20@users.noreply.github.com>
2023-09-11 20:12:21 +0530

    prac ques

---
+++ CPP/13_File_Handing/prac.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+  ofstream onFile;
+  string str;
+  onFile.open("prac1.txt");
+  getline(cin,str);
+  onFile << str;
+}
