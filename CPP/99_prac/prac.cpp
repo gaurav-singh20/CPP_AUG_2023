76b93c265d5d334a0264e3e1be709a8a81b358f0
Gaurav Singh <120627440+gaurav-singh20@users.noreply.github.com>
2023-09-14 19:48:01 +0530

    sep14

--- CPP/99_prac/prac.cpp
+++ CPP/99_prac/prac.cpp
@@ -1,35 +1,13 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-struct Node {
-    int data;
-    struct Node* next;
-};
-
-void traverseList(struct Node* head) {
-    int count = 0;
-    while (head != NULL) {
-        count++;
-        head = head->next;
-    }
-    cout << count;
-}
-
 int main() {
-    struct Node* head = NULL;
-    struct Node* second = NULL;
-    struct Node* third = NULL;
-
-    head = (struct Node*)malloc(sizeof(struct Node));
-    second = (struct Node*)malloc(sizeof(struct Node));
-    third = (struct Node*)malloc(sizeof(struct Node));
-
-    head->data = 1;
-    head->next = second;
-    second->data = 2;
-    second->next = third;
-    third->data = 3;
-    third->next = NULL;
-
-    traverseList(head);
+    string str1 = "Hello";
+    string str2 = "World";
+    str1[0] = 'h';
+    cout << str1 << endl;
+    str2[0] = 'n';
+    cout << str2 << endl;
+    return 0;
 }
---
+++ CPP/99_prac/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+cout << ++(c1.speed);
+    // c1.*pSpeed = 2;
+    // cout << --(c1.speed);
+    // c1.speed = 1;
+    // return 0;
