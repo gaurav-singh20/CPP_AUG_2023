f0869c7b56255f057b689b41d38b94445a5307fd
Gaurav Singh <120627440+gaurav-singh20@users.noreply.github.com>
2023-09-30 20:02:29 +0530

    ..

---
+++ DSA/03_Stack/06prblm4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stack>
+#include <math.h>
+//[](){}
+using namespace std;
+
+int preInfix(string s){
+  for(;;){
+    if(int i=(s.size()-1);i<=0;i--){
+
+    }
+
+    else(){
+
+    }
+
+    switch(expression) {
+      case x:
+      
+      break;
+      case y:
+      
+      break;
+      default:
+      
+    }
+  }
+
+}
+ 
+
+int main(){
+
+
+}
