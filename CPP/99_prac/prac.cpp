44dc7cd566d3522b17871cba833786edaededcc0
Gaurav Singh <120627440+gaurav-singh20@users.noreply.github.com>
2023-10-03 19:56:50 +0530

    new (memory allocation)

--- CPP/99_prac/prac.cpp
+++ CPP/99_prac/prac.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
-class MyClass {
-public:
-    int num;
 
-    MyClass() {
-        num = 10;
-    }
-
-    MyClass(const MyClass& other) {
-        num = other.num * 2;
-    }
+struct Student {
+    string name;
+    int rollNo;
+    float marks;
 };
 
 int main() {
-    MyClass obj1;
-    MyClass obj2 = obj1;
-    cout << obj2.num;
+    fstream file("students.txt", ios::in | ios::out | ios::app);
+
+    if (file) {
+        Student s1{"John", 101, 85.5};
+        Student s2{"Jane", 102, 92.0};
+
+        file << s1.name << " " << s1.rollNo << " " << s1.marks << endl;
+        file << s2.name << " " << s2.rollNo << " " << s2.marks << endl;
+
+        file.seekg(0);
+        Student readS1;
+        Student readS2;
+
+        file >> readS1.name >> readS1.rollNo >> readS1.marks;
+        file >> readS2.name >> readS2.rollNo >> readS2.marks;
+
+        cout << readS1.name << " scored " << readS1.marks << endl;
+        cout << readS2.name << " scored " << readS2.marks << endl;
+
+        file.close();
+    } else {
+        cout << "File not found.";
+    }
+
     return 0;
 }
