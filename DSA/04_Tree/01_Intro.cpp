9d8d28c260f85d6550da45eaff1354652ad622e3
Gaurav Singh <120627440+gaurav-singh20@users.noreply.github.com>
2023-10-07 12:41:21 +0530

    tree notes

---
+++ DSA/04_Tree/01_Intro.cpp
@@ -0,0 +1,11 @@
+//root- sabka baap
+//children, Parent
+//Sibling- nodes sharing the same parent
+//Leaf- Node with no child.
+//Ancestor- kisi bhi node ke sare uper wale
+//Descendent- sabhi niche wale
+//no. of edges= no. of nodes - 1. (e=n-1)
+//depth- no. of edges traversed to reach to the node from the root.
+//height of a node- no. of edges travelled to reach the farthest leaf from the node.
+
+//binary tree- a tree in which each node have atmost two children.
