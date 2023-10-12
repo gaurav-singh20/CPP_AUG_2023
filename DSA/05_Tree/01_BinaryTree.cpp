2d4e3140885c08cabd4456afea78f7db0f9f7865
Gaurav Singh <120627440+gaurav-singh20@users.noreply.github.com>
2023-10-12 19:50:34 +0530

    preorder inorder and post order

--- DSA/05_Tree/01_BinaryTree.cpp
+++ DSA/05_Tree/01_BinaryTree.cpp
@@ -13,10 +13,36 @@ class Node{
     right=NULL;
   }
 
-  void displayTree(Node* root){
-    
-  }
 };
+
+//preorder- Root-Left-Right
+void preorder(Node* root){
+  if(root==NULL) return;
+
+  cout << root->data << " ";
+
+  preorder(root->left);
+  preorder(root->right);
+}
+
+//inorder- Left-Root-Right
+void inorder(Node* root){
+  if(root==NULL) return;
+
+  inorder(root->left);
+  
+  cout << root->data << " ";
+  inorder(root->right);
+}
+
+//postorder- Left-Right-Root
+void postorder(Node* root){
+  if(root==NULL) return;
+
+  postorder(root->left);
+  postorder(root->right);
+  cout << root->data << " ";
+}
 
 int main(){
   Node* root = new Node(1);
@@ -27,4 +53,10 @@ int main(){
   root->left->right= new Node(5);
   root->right->left=new Node(6);
   root->right->right=new Node(7);
+
+  preorder(root);
+  cout << endl;
+  inorder(root);
+  cout << endl;
+  postorder(root);
 }
