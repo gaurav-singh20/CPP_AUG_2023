4f2b8c9e13fcbf2ee674b1de574ab5b17fdd7aa9
Gaurav Singh <120627440+gaurav-singh20@users.noreply.github.com>
2023-10-26 20:38:30 +0530

    practice

--- CPP/99_prac/prac.cpp
+++ CPP/99_prac/prac.cpp
@@ -1,37 +1,19 @@
 #include <iostream>
-#include <fstream>
 using namespace std;
 
-struct Student {
-    string name;
-    int rollNo;
-    float marks;
+class Point {
+public:
+    int x, y;
+    Point(int x_val, int y_val) : x(x_val), y(y_val) {}
+    Point operator-(Point& other) {
+        return Point(x - other.x, y - other.y);
+    }
 };
 
 int main() {
-    fstream file("students.txt", ios::in | ios::out | ios::app);
-
-    if (file) {
-        Student s1{"John", 101, 85.5};
-        Student s2{"Jane", 102, 92.0};
-
-        file << s1.name << " " << s1.rollNo << " " << s1.marks << endl;
-        file << s2.name << " " << s2.rollNo << " " << s2.marks << endl;
-
-        file.seekg(0);
-        Student readS1;
-        Student readS2;
-
-        file >> readS1.name >> readS1.rollNo >> readS1.marks;
-        file >> readS2.name >> readS2.rollNo >> readS2.marks;
-
-        cout << readS1.name << " scored " << readS1.marks << endl;
-        cout << readS2.name << " scored " << readS2.marks << endl;
-
-        file.close();
-    } else {
-        cout << "File not found.";
-    }
-
+    Point p1(5, 10);
+    Point p2(2, 6);
+    Point result = p1 - p2;
+    cout << result.x << ", " << result.y;
     return 0;
 }
